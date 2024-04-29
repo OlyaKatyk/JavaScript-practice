822e8bb2979468b7a8b011436bbb59a7ceb7fc87
olyak <olya.katyk@gmail.com>
2024-04-29 21:56:35 +0200

    done hw1

---
+++ HW1/hw1.js
@@ -0,0 +1,47 @@
+//Завдання 1
+let h = 'hello';
+let o= 'owu';
+let com='com';
+let ua='ua';
+let a1= 1;
+let a2= 10;
+let a3= -999;
+let a4= 123;
+let a5= 3.14;
+let a6= 2.7;
+let a7= 16;
+let e= true;
+let f= false;
+console.log(h);
+console.log(o);
+console.log(com);
+console.log(ua);
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(e);
+console.log(f);
+
+
+//Завдання 2
+let firstName='Olha'
+let middleName='Olehivna'
+let lastName='Katyk'
+
+let person=`${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+//Завдання 3
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(a)
+console.log(b)
+console.log(c)
